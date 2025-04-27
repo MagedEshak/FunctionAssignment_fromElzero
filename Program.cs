e489da6bd65859d09cadcda5f1594bf8e9874106
Maged Eshak <magedeshak36@gmail.com>
2025-04-27 22:22:48 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+// --------------------------- 1 -------------------------------------
+
+int funStatus(int num1, int num2)
+{
+    if (num1 == num2)
+    {
+        return num1 + num2;
+    }
+    
+    else if (num1 > num2)
+    {
+        return num1 - num2;
+    }
+
+    else if (num2 > num1)
+    {
+        return num2 - num1;
+    }
+    return 0;
+}
+
+int result = funStatus(100, 100);
+
+Console.WriteLine(result);
